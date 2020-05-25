1d5206c09bdcaa0ae55df749897a3c032502169e
Gustavo Flores <gustavofc65@gmail.com>
2020-05-25 14:34:45 -0500

    [ADD] first commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "fix error views mig",
+    "author": "testmodules",
+    "summary": """
+    Views that for some reason are not created in the requiez module are moved to this repository
+    """,
+    "website": "https://www.gruporequiez.com",
+    "license": "LGPL-3",
+    "category": "",
+    "version": "12.0.0.0.1",
+    "depends": [
+        "requiez",
+        "account",
+    ],
+    "data": [
+        "view/res_partner.xml",
+        "view/account_invoice.xml",
+    ],
+    "demo": [
+    ],
+    "test": [
+    ],
+    "qweb": [],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
